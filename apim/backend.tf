d49971848adc30adceaf05a88db6fe661a955c40
Your GitHub Username <Your GitHub Email>
2025-02-20 06:12:30 +0100

    remove backend

--- apim/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name = "dev-rg"   
-    storage_account_name = "tdevstatestorage"
-    container_name = "dev-tfstate-container"
-    key = "apims.tfstate"
-  }
-}
